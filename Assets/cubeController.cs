89dfca845aa80afc24e39672534f668a0a149884
Eliza Koster <ekmanga101@gmail.com>
2017-11-08 15:04:22 -0500

    Part 4

---
+++ Assets/cubeController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeController : MonoBehaviour {
+	public int x,y;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnMouseDown() {
+		gameController.ProcessClick(gameObject,x,y);
+	
+	}
+}
